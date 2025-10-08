388ee7bac7154a15f75501b9f59c933c9307cccd
XinYan <103906642+XinYanC@users.noreply.github.com>
2025-10-07 20:03:40 -0400

    Add input checks to create function to require for city name

--- cities/tests/test_queries.py
+++ cities/tests/test_queries.py
@@ -1,8 +1,14 @@
+# To run test: PYTHONPATH=$(pwd) pytest -v cities/tests/test_queries.py
+
 import pytest
 
 import cities.queries as qry
 
 
-def test_create():
+def test_good_create():
+    assert qry.create(qry.SAMPLE_CITY) == qry.SUCCESS
+
+
+def test_create_bad_param_type():
     with pytest.raises(ValueError):
         qry.create(17)
