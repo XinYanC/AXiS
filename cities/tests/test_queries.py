73f93d8361679200cf9cdb53b5c7653bc14e1086
XinYan <103906642+XinYanC@users.noreply.github.com>
2025-10-14 21:28:46 -0400

    Update to use a city_cache in the create function and updated tests

--- cities/tests/test_queries.py
+++ cities/tests/test_queries.py
@@ -5,8 +5,26 @@ import pytest
 import cities.queries as qry
 
 
+@pytest.mark.skip('This is an example of a bad test!')
+def test_bad_test_for_num_cities():
+    assert qry.num_cities() == len(qry.cities)
+
+
+def test_num_cities():
+    old_count = qry.num_cities()
+    qry.create(qry.SAMPLE_CITY)
+    assert qry.num_cities() == old_count + 1
+
+
 def test_good_create():
-    assert qry.create(qry.SAMPLE_CITY) == qry.SUCCESS
+    old_count = qry.num_cities()
+    assert qry.create(qry.SAMPLE_CITY) >= 1
+    assert qry.num_cities() > old_count
+
+
+def test_create_bad_name():
+    with pytest.raises(ValueError):
+        qry.create({})
 
 
 def test_create_bad_param_type():
